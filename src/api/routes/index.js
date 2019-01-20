38e623c193bf7e783d0fc4e784e661aa00b6b16d
Fabricio Nogueira <nogsantos@gmail.com>
2019-01-20 03:53:01 -0200

    feat(view): Marko view
    Setup marko as view provider server side render

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,8 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+require("marko/node-require").install();
+require("marko/express");
 
-router.get('/', (req, res) => {
-    res.status(200).send('This was a success? Status code: 200');
+var indexTemplate = require("../views/index.marko");
+
+router.get("/", (req, res) => {
+	console.log("Status", "Woks on route /");
+	res.marko(indexTemplate);
+});
+
+router.get("/hello", (req, res) => {
+	console.log("Status", "Woks on route /hello");
+	res.status(200).send("This was a success? Status code: 200");
 });
 
 module.exports = router;
